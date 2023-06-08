46a4fed724646d26c718d1296bdd7f6017725732
llh4github <lilinhong_coding@foxmail.com>
2023-06-08 15:21:57 +0800

    fix: 引用生成的Input对象
    包位置错误

--- core/src/main/kotlin/com/github/llh4github/jimmerhelper/core/generator/util.kt
+++ core/src/main/kotlin/com/github/llh4github/jimmerhelper/core/generator/util.kt
@@ -1,5 +1,6 @@
 package com.github.llh4github.jimmerhelper.core.generator
 
+import com.github.llh4github.jimmerhelper.core.common.inputDtoPkgName
 import com.github.llh4github.jimmerhelper.core.common.inputDtoSuffix
 import com.github.llh4github.jimmerhelper.core.dto.ClassInfoDto
 import com.github.llh4github.jimmerhelper.core.dto.FieldInfoDto
@@ -18,28 +19,32 @@ internal fun properties(fields: List<FieldInfoDto>, init: Boolean = false): List
     return fields
         .filter { !it.isIdViewListField } // 暂不处理此类型字段
         .map {
-        val type = propertyType(it)
-        val propertySpec = if (it.isList) {
-            PropertySpec.builder(it.name, type)
-                .mutable(true)
-        } else {
-            PropertySpec.builder(it.name, type)
-                .mutable(true)
-        }
-        if (init) {
-            return@map propertySpec.initializer(propertyDefaultValue(it)).build()
-        } else {
-            return@map propertySpec.build()
-        }
-    }.toList()
+            val type = propertyType(it)
+            val propertySpec = if (it.isList) {
+                PropertySpec.builder(it.name, type)
+                    .mutable(true)
+            } else {
+                PropertySpec.builder(it.name, type)
+                    .mutable(true)
+            }
+            if (init) {
+                return@map propertySpec.initializer(propertyDefaultValue(it)).build()
+            } else {
+                return@map propertySpec.build()
+            }
+        }.toList()
 }
 
 internal fun propertyType(field: FieldInfoDto): TypeName {
     return if (field.isRelationField) {
-        if (field.isList)
-            ClassName(field.typePackage, field.typeName)
+        if (field.isList) {
+            val clazz = ClassName(field.typePackage, field.typeName)
                 .parameterizedBy(ClassName(field.typeParamTypeInputDtoPkgName!!, field.typeParamTypeInputDtoName!!))
-        else ClassName(field.typePackage, field.typeName + inputDtoSuffix).copy(true)
+//            logger.info("type argument : ${clazz.typeArguments}")
+            clazz
+        } else {
+            ClassName(field.typePackage+".${inputDtoPkgName}", field.typeName + inputDtoSuffix).copy(true)
+        }
     } else {
         ClassName(field.typePackage, field.typeName).copy(true)
     }
