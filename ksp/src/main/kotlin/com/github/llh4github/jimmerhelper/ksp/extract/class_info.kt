6b2f9ac6fd14a0fa24f220ea56620c5880f5d6e9
llh4github <lilinhong_coding@foxmail.com>
2023-06-16 15:35:09 +0800

    style: 整理代码

--- ksp/src/main/kotlin/com/github/llh4github/jimmerhelper/ksp/extract/class_info.kt
+++ ksp/src/main/kotlin/com/github/llh4github/jimmerhelper/ksp/extract/class_info.kt
@@ -1,5 +1,6 @@
 package com.github.llh4github.jimmerhelper.ksp.extract
 
+import com.github.llh4github.jimmerhelper.ToJimmerEntity
 import com.github.llh4github.jimmerhelper.ksp.common.*
 import com.github.llh4github.jimmerhelper.ksp.dto.ClassInfoDto
 import com.google.devtools.ksp.getAllSuperTypes
@@ -14,27 +15,44 @@ fun extractJimmerModelInfo(sequence: Sequence<KSFile>): List<ClassInfoDto> {
     return sequence
         .flatMap { it.declarations.filterIsInstance<KSClassDeclaration>() }
         .filter { isJimmerModelClass(it) }
-        .map { extractSingleClassInfo(it) }
+        .map { extractClassInfoAndCollectSupperClass(it) }
         .toList()
 }
 
+fun extractMyAnnoClassInfo(sequence: Sequence<KSFile>): List<ClassInfoDto> {
+    return sequence
+        .flatMap { it.declarations.filterIsInstance<KSClassDeclaration>() }
+        .filter { it.annotations.hasAnno(ToJimmerEntity::class) }
+        .map { extractClassInfo(it) }
+        .toList()
+}
 
 private fun isJimmerModelClass(declaration: KSClassDeclaration): Boolean {
     return declaration.annotations.hasAnyAnno(JimmerEntityAnno)
 }
 
 
-private fun extractSingleClassInfo(classDeclaration: KSClassDeclaration): ClassInfoDto {
-    val isSupperClass = isSupperClass(classDeclaration)
+/**
+ * 提取类信息
+ */
+private fun extractClassInfo(classDeclaration: KSClassDeclaration): ClassInfoDto {
     val fields = extractFieldInfo(classDeclaration.getAllProperties())
-    val dto = ClassInfoDto(
+    return ClassInfoDto(
         packageName = classDeclaration.packageName.asString(),
         className = classDeclaration.className().simpleName,
         doc = classDeclaration.docString,
-        isSupperClass = isSupperClass,
+        isSupperClass = isSupperClass(classDeclaration),
         fields = fields,
         parentNames = extractParentNames(classDeclaration)
     )
+}
+
+/**
+ * 提取类信息并收集父类
+ */
+private fun extractClassInfoAndCollectSupperClass(classDeclaration: KSClassDeclaration): ClassInfoDto {
+    val isSupperClass = isSupperClass(classDeclaration)
+    val dto = extractClassInfo(classDeclaration)
     if (isSupperClass)
         SUPER_CLASS_MAP[dto.className] = dto
     return dto
