620e0eaa93bcb4219d9a18cbc0e2c8b10ced314f
llh <lilinhong_coding@foxmail.com>
2023-07-18 14:32:46 +0800

    build: signing插件配置

--- ksp/build.gradle.kts
+++ ksp/build.gradle.kts
@@ -82,14 +82,14 @@ publishing {
     }
 }
 signing {
-//    val keyId = System.getenv("GPG_KEY_ID")
-//    val secretKey = System.getenv("GPG_SECRET_KEY")
-//    val password = System.getenv("GPG_PASSWORD")
-//
-//    setRequired {
-//        !project.version.toString().endsWith("SNAPSHOT")
-//    }
-//
-//    useInMemoryPgpKeys(keyId, secretKey, password)
+    val keyId = System.getenv("GPG_KEY_ID")
+    val secretKey = System.getenv("GPG_SECRET_KEY")
+    val password = System.getenv("GPG_PASSWORD")
+
+    setRequired {
+        !project.version.toString().endsWith("SNAPSHOT")
+    }
+
+    useInMemoryPgpKeys(keyId, secretKey, password)
     sign(publishing.publications["dist"])
 }
