883b23861388d68795a74687f19c17649c1f9444
llh <lilinhong_coding@foxmail.com>
2023-09-12 16:27:39 +0800

    feat: 实现input接口

--- jimmer-helper-ksp/src/main/kotlin/io/github/llh4github/jimmer/ksp/generator/InputSupportDtoGen.kt
+++ jimmer-helper-ksp/src/main/kotlin/io/github/llh4github/jimmer/ksp/generator/InputSupportDtoGen.kt
@@ -1,7 +1,10 @@
 package io.github.llh4github.jimmer.ksp.generator
 
 import com.squareup.kotlinpoet.*
+import com.squareup.kotlinpoet.ParameterizedTypeName.Companion.parameterizedBy
+import io.github.llh4github.jimmer.ksp.common.JimmerMember
 import io.github.llh4github.jimmer.ksp.dto.ClassDefinition
+import io.github.llh4github.jimmer.ksp.dto.FieldDefinition
 
 /**
  * input-support-dto类生成逻辑
@@ -11,6 +14,10 @@ import io.github.llh4github.jimmer.ksp.dto.ClassDefinition
  */
 object InputSupportDtoGen : AbstractGenerator() {
     private lateinit var allClass: List<ClassDefinition>
+    private val superClass: List<ClassDefinition> by lazy {
+        allClass.filter { it.isMappedSuperclass }.toList()
+    }
+
     override fun generate(list: List<ClassDefinition>): List<FileSpec> {
         allClass = list
         return list
@@ -31,7 +38,10 @@ object InputSupportDtoGen : AbstractGenerator() {
                 .addKdoc(comment)
                 .addModifiers(KModifier.DATA)
                 .primaryConstructor(constructFun)
+                .addSuperinterface(jimmerInputInterface(definition))
+                .addSuperinterfaces(baseDtoSuperInterface(definition))
                 .addProperties(constructorProperties)
+                .addFunction(overrideToEntityMethod(definition))
                 .addAnnotation(suppressWarns)
                 .build()
         ).build()
@@ -66,12 +76,58 @@ object InputSupportDtoGen : AbstractGenerator() {
         if (parentName.isEmpty()) {
             return false
         }
-        return allClass.filter { it.isMappedSuperclass }
+        return superClass
             .filter { parentName.contains(it.name) }
             .flatMap { it.fields }
             .map { it.name }
             .any { it == fieldName }
     }
 
+    private fun baseDtoSuperInterface(definition: ClassDefinition): List<ClassName> {
+        return superClass
+            .filter { definition.parentNames.contains(it.name) }
+            .map { ClassName(it.inputDtoPkg, it.inputDtoClassName) }
+            .toList()
+    }
 
+    private fun jimmerInputInterface(definition: ClassDefinition): ParameterizedTypeName {
+        return JimmerMember.inputInterface.parameterizedBy(ClassName(definition.packageName, definition.name))
+    }
+
+    private fun overrideToEntityMethod(definition: ClassDefinition): FunSpec {
+
+        val builder = FunSpec.builder("toEntity")
+            .addModifiers(KModifier.OVERRIDE)
+            .returns(ClassName(definition.packageName, definition.name))
+            .addCode("return ")
+            .addStatement("%M(%L::class).by{", JimmerMember.newFun, definition.name)
+        definition.fields.forEach {
+            if (it.jimmerFieldRestrict.isIdViewListField) {
+                return@forEach
+            }
+            if (it.jimmerFieldRestrict.isRelation) {
+                relationFields(builder, it, definition)
+                return@forEach
+            }
+            builder
+                .addStatement("this@%L.%L?.let{", definition.inputDtoClassName, it.name)
+                .addStatement("%L = it", it.name)
+                .addStatement("}")
+        }
+        return builder.addStatement("}").build()
+    }
+
+    private fun relationFields(builder: FunSpec.Builder, field: FieldDefinition, definition: ClassDefinition) {
+        if (field.isListField) {
+            builder
+                .addStatement("this@%L.%L.forEach{", definition.inputDtoClassName, field.name)
+                .addStatement("%L().addBy{ it.toEntity() }", field.name)
+                .addStatement("}")
+        } else {
+            builder
+                .addStatement("this@%L.%L?.let{", definition.inputDtoClassName, field.name)
+                .addStatement("%L = it.toEntity()", field.name)
+                .addStatement("}")
+        }
+    }
 }
